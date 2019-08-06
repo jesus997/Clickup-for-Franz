d7c00429b78f7176fb819cb86eabb53230b5de5e
Jesús Magallón <magallonj23@gmail.com>
2019-08-06 16:54:46 -0500

    First commit

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# Slack for Franz
-This is the official Franz recipe for Slack
+# Clickup for Franz
+Simple integration of Clickup to Franz.
 
 ### How to create your own Franz recipes:
 * [Read the documentation](https://github.com/meetfranz/plugins)
--- webview.js
+++ webview.js
@@ -1,53 +1,13 @@
-import { ipcRenderer } from 'electron';
 import path from 'path';
-
-const getTeamIcon = function getTeamIcon(count = 0) {
-  let countTeamIconCheck = count;
-  let bgUrl = null;
-
-  const teamMenu = document.querySelector('#team_menu');
-  if (teamMenu) {
-    teamMenu.click();
-
-    const icon = document.querySelector('.team_icon');
-    if (icon) {
-      bgUrl = window.getComputedStyle(icon, null).getPropertyValue('background-image');
-      bgUrl = /^url\((['"]?)(.*)\1\)$/.exec(bgUrl);
-      bgUrl = bgUrl ? bgUrl[2] : '';
-    }
-
-    setTimeout(() => {
-      document.querySelector('.team_menu').remove();
-      document.querySelector('#msg_input .ql-editor').focus();
-    }, 10);
-  }
-
-  countTeamIconCheck += 1;
-
-  if (bgUrl) {
-    ipcRenderer.sendToHost('avatar', bgUrl);
-  } else if (countTeamIconCheck <= 5) {
-    setTimeout(() => {
-      getTeamIcon(countTeamIconCheck + 1);
-    }, 2000);
-  }
-};
-
-const SELECTOR_CHANNELS_UNREAD = '.p-channel_sidebar__channel--unread:not(.p-channel_sidebar__channel--muted)';
 
 module.exports = (Franz) => {
   const getMessages = () => {
-    const directMessages = document.querySelectorAll(`${SELECTOR_CHANNELS_UNREAD} .p-channel_sidebar__badge`).length;
-    const allMessages = document.querySelectorAll(SELECTOR_CHANNELS_UNREAD).length - directMessages;
+    const directMessages = document.querySelectorAll(`.cu-icon.cu-notification-alert__dot.ng-star-inserted`).length;
 
     // set Franz badge
-    Franz.setBadge(directMessages, allMessages);
+    Franz.setBadge(directMessages);
   };
   Franz.loop(getMessages);
-
-  setTimeout(() => {
-    getTeamIcon();
-  }, 4000);
 
   // inject franz.css stylesheet
   Franz.injectCSS(path.join(__dirname, 'service.css'));
